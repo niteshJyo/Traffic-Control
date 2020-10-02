feb3149f5016801e284fed46c3efd57cf5d56408
niteshJyo <niteshjyotishi@gmail.com>
2020-10-03 02:49:10 +0530

    Changed the aesthetics of the app

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'dart:ui';
 import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 /// The [SharedPreferences] key to access the alarm fire count.
 const String countKey = 'count';
@@ -19,11 +20,7 @@ final ReceivePort port = ReceivePort();
 SharedPreferences prefs;
 
 Future<void> main() async {
-  // TODO(bkonyi): uncomment
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Register the UI isolate's SendPort to allow for communication from the
-  // background isolate.
   IsolateNameServer.registerPortWithName(
     port.sendPort,
     isolateName,
@@ -35,14 +32,16 @@ Future<void> main() async {
   runApp(AlarmManagerExampleApp());
 }
 
-/// Example app for Espresso plugin.
 class AlarmManagerExampleApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: _AlarmHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Traffic Control',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: _AlarmHomePage(title: 'Traffic Control'),
     );
   }
 }
@@ -56,95 +55,78 @@ class _AlarmHomePage extends StatefulWidget {
 }
 
 class _AlarmHomePageState extends State<_AlarmHomePage> {
-  int _counter = 0;
-
   @override
   void initState() {
     super.initState();
     AndroidAlarmManager.initialize();
 
-    // Register for events from the background isolate. These messages will
-    // always coincide with an alarm firing.
-    port.listen((_) async => await _incrementCounter());
+    port.listen((_) async => await playMusic());
   }
 
-  Future<void> _incrementCounter() async {
-    print('Increment counter!');
-
-    // Ensure we've loaded the updated count from the background isolate.
+  Future<void> playMusic() async {
+    print('Play');
     await prefs.reload();
-
-    setState(() {
-      _counter++;
-    });
   }
 
-  // The background
   static SendPort uiSendPort;
 
-  // The callback for our alarm
   static Future<void> callback() async {
     print('Alarm fired!');
 
-    // Get the previous cached count and increment it.
     final prefs = await SharedPreferences.getInstance();
     int currentCount = prefs.getInt(countKey);
     await prefs.setInt(countKey, currentCount + 1);
 
-    // This will be null if we're running in the background.
     uiSendPort ??= IsolateNameServer.lookupPortByName(isolateName);
     uiSendPort?.send(null);
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO(jackson): This has been deprecated and should be replaced
-    // with `headline4` when it's available on all the versions of
-    // Flutter that we test.
-    // ignore: deprecated_member_use
-    final textStyle = Theme.of(context).textTheme.display1;
     return Scaffold(
+      backgroundColor: Colors.red[50],
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(widget.title, style: GoogleFonts.amaranth(fontSize: 22)),
+        backgroundColor: Colors.redAccent,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 100),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Alarm fired $_counter times',
-              style: textStyle,
-            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(
-                  'Total alarms fired: ',
-                  style: textStyle,
-                ),
-                Text(
-                  prefs.getInt(countKey).toString(),
-                  key: ValueKey('BackgroundCountText'),
-                  style: textStyle,
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  color: Colors.redAccent,
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 70),
+                  elevation: 0.5,
+                  splashColor: Colors.white70,
+                  child: Text('Set One Hour Timer',
+                      style: GoogleFonts.amaranth(
+                        shadows: <Shadow>[Shadow(blurRadius: 25, color: Colors.black87,)],
+                        fontSize: 22,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      )),
+                  key: ValueKey('RegisterOneShotAlarm'),
+                  onPressed: () async {
+                    await AndroidAlarmManager.oneShot(
+                      const Duration(seconds: 360),
+                      // Ensure we have a unique alarm ID.
+                      Random().nextInt(pow(2, 31)),
+                      callback,
+                      exact: true,
+                      wakeup: true,
+                    );
+                  },
                 ),
               ],
-            ),
-            RaisedButton(
-              child: Text(
-                'Schedule OneShot Alarm',
-              ),
-              key: ValueKey('RegisterOneShotAlarm'),
-              onPressed: () async {
-                await AndroidAlarmManager.oneShot(
-                  const Duration(seconds: 5),
-                  // Ensure we have a unique alarm ID.
-                  Random().nextInt(pow(2, 31)),
-                  callback,
-                  exact: true,
-                  wakeup: true,
-                );
-              },
-            ),
+            )
           ],
         ),
       ),
