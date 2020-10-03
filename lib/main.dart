279fb701a7560dfd6fa5305bced4fbd2ab74cc4d
niteshJyo <niteshjyotishi@gmail.com>
2020-10-03 18:12:31 +0530

    Changed app background and counter function

--- lib/main.dart
+++ lib/main.dart
@@ -59,23 +59,18 @@ class _AlarmHomePageState extends State<_AlarmHomePage> {
   void initState() {
     super.initState();
     AndroidAlarmManager.initialize();
-
-    port.listen((_) async => await playMusic());
+    //port.listen((_) async => await playMusic());
   }
 
-  Future<void> playMusic() async {
-    print('Play');
-    await prefs.reload();
-  }
+  //Future<void> playMusic() async {
+    //print('Play');
+    //await prefs.reload();
+  //}
 
   static SendPort uiSendPort;
 
   static Future<void> callback() async {
     print('Alarm fired!');
-
-    final prefs = await SharedPreferences.getInstance();
-    int currentCount = prefs.getInt(countKey);
-    await prefs.setInt(countKey, currentCount + 1);
 
     uiSendPort ??= IsolateNameServer.lookupPortByName(isolateName);
     uiSendPort?.send(null);
@@ -89,47 +84,59 @@ class _AlarmHomePageState extends State<_AlarmHomePage> {
         title: Text(widget.title, style: GoogleFonts.amaranth(fontSize: 22)),
         backgroundColor: Colors.redAccent,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 100),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
+      body: Stack(children: [
+        Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/trafficcontrolBG.png"),
+                    fit: BoxFit.fill))),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 150),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    color: Colors.redAccent,
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 65),
+                    elevation: 0.5,
+                    splashColor: Colors.white70,
+                    child: Text('Set One Hour Timer',
+                        style: GoogleFonts.amaranth(
+                          shadows: <Shadow>[
+                            Shadow(
+                              blurRadius: 25,
+                              color: Colors.black87,
+                            )
+                          ],
+                          fontSize: 22,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        )),
+                    key: ValueKey('RegisterOneShotAlarm'),
+                    onPressed: () async {
+                      await AndroidAlarmManager.oneShot(
+                        const Duration(seconds: 3),
+                        // Ensure we have a unique alarm ID.
+                        Random().nextInt(pow(2, 31)),
+                        callback,
+                        exact: true,
+                        wakeup: true,
+                      );
+                    },
                   ),
-                  color: Colors.redAccent,
-                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 70),
-                  elevation: 0.5,
-                  splashColor: Colors.white70,
-                  child: Text('Set One Hour Timer',
-                      style: GoogleFonts.amaranth(
-                        shadows: <Shadow>[Shadow(blurRadius: 25, color: Colors.black87,)],
-                        fontSize: 22,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                      )),
-                  key: ValueKey('RegisterOneShotAlarm'),
-                  onPressed: () async {
-                    await AndroidAlarmManager.oneShot(
-                      const Duration(seconds: 360),
-                      // Ensure we have a unique alarm ID.
-                      Random().nextInt(pow(2, 31)),
-                      callback,
-                      exact: true,
-                      wakeup: true,
-                    );
-                  },
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+                ],
+              )
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
