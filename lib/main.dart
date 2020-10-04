05c36d0340fd763b678ec4eae0ce1f8cf09f94f8
niteshJyo <niteshjyotishi@gmail.com>
2020-10-04 17:23:27 +0530

    Added audio player

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+  
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 /// The [SharedPreferences] key to access the alarm fire count.
 const String countKey = 'count';
